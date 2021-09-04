2dc6f9d692e0d07bd892d244427ac449a68e0290
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-09-03 23:07:00 -0300

    Criado o contexto da aplicação

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from '../components/Header';
 import '../styles/Home.css';
 
 function Home() {
+  useEffect(() => {
+    
+  }, [])
   return (
     <section className="hero">
       <Header />
