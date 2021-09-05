2071c50e141c1beffef29018b287ccd0c89f6009
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-09-05 11:15:20 -0300

    Feito teste da HomePage na parte do Hero

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -2,11 +2,11 @@ import { render } from '@testing-library/react';
 import App from '../App';
 
 
-describe('Renderizando elementos na tela', () => {
+describe('Renderizando elementos do Hero na HomePage', () => {
   it('Verifica o texto no hero', () => {
     const { getByText } = render(<App />);
     const texto = getByText(
-      'Easily create or join a local nanny share with Hapu'
+      'Create the childcare you need at a price you can afford'
     );
     expect(texto).toBeInTheDocument();
   });
@@ -16,6 +16,11 @@ describe('Renderizando elementos na tela', () => {
 
     const link = getAllByRole('link');
     expect(link[0]).toHaveTextContent('Create Your Nanny Share');
+    expect(link[1]).toHaveTextContent('Browse Shares');
+    expect(link[2]).toHaveTextContent('Our Story');
+  });
+
+  it('Verifica o model de Cadastro/login', () => {
     
   })
 })
