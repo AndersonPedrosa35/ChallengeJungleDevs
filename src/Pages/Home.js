a9d042e213b29a65f35b9d077929cb772c7b6d2c
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-09-05 21:55:21 -0300

    Separação entre a descrição e o market

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,9 +4,15 @@ import Hero from '../components/Hero';
 
 export default function Home() {
   return (
-    <div>
+    <div className="home">
       <Hero />
       <About />
+      <hr className="separator"/>
+      <section className="market">
+        <h2>Are you a parent without a nanny and looking to share?</h2>
+        <p>Leave us your name and email and we’ll 
+          update you as soon as a share becomes available in your area!</p>
+      </section>
     </div>
   )
 }
